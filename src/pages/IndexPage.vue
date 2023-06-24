ee23fe9001b7c031583be5bf11b3dfde9428d5b2
Jennifer Bland <10262857+ratracegrad@users.noreply.github.com>
2023-06-24 19:04:03 -0400

    copying example of using dialog from youtube shorts. Only change is don't want a component for button so copied basic styles to create a fixed button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Dialog from '@/components/DialogComponent.vue';
+
 // useRoute, useHead, and HelloWorld are automatically imported. See vite.config.ts for details.
 const route = useRoute();
 
@@ -15,6 +17,8 @@ useHead({
     },
   ],
 });
+
+const isOpen = ref(false);
 </script>
 import FooterComponent from '@/components/FooterComponent.vue'
 
@@ -36,25 +40,25 @@ import FooterComponent from '@/components/FooterComponent.vue'
             from previously lost ships.
           </p>
           <!-- DELETE BElOw -->
-          <p>
-            TODO ITEMS
-          </p>
+          <p>TODO ITEMS</p>
           <ul>
             <li>add dialog</li>
             <li>set current step in Pinia</li>
             <li>have the following steps for the flow of game play</li>
-              <ol>
-                <li>Get grid size</li>
-                <li>Get Ship info</li>
-                <li>Get Ship's movements</li>
-                <li>Get Output</li>
-                <li>Play again or quit game</li>
-              </ol>
+            <ol>
+              <li>Get grid size</li>
+              <li>Get Ship info</li>
+              <li>Get Ship's movements</li>
+              <li>Get Output</li>
+              <li>Play again or quit game</li>
+            </ol>
           </ul>
+          <!-- DELETE ABOVE -->
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <button
               type="button"
               class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              @click="isOpen = true"
             >
               Get started
             </button>
@@ -67,6 +71,36 @@ import FooterComponent from '@/components/FooterComponent.vue'
         </div>
       </main>
       <FooterComponent></FooterComponent>
+
+      <!-- Use Dialog Component -->
+      <Dialog :open="isOpen" @close="isOpen = false">
+        <!-- Dialog Header -->
+        <template #header>
+          <h2 class="font-bold text-lg text-center">Dialog Title</h2>
+        </template>
+
+        <!-- Dialog Content -->
+        <template #content>
+          <p class="text-gray-600 leading-relaxed text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </template>
+
+        <!-- Dialog Footer -->
+        <template #footer>
+          <div class="text-center space-x-4">
+            <button
+              class="button text-center transition-all duration-200 cursor-pointer rounded-full text-white bg-purple-500 hover:bg-purple-600 focus:bg-purple-600 px-6 py-2.5"
+              @click="isOpen = false"
+            >
+              Close
+            </button>
+          </div>
+        </template>
+      </Dialog>
     </div>
   </div>
 </template>
