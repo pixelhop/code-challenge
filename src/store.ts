1af4b3725b3a383dadd9aff28ec52036c7e87baa
Jennifer Bland <10262857+ratracegrad@users.noreply.github.com>
2023-06-24 19:13:21 -0400

    Removed default code that came with starter template. Added in currentStep ins state and an action to increment it

--- src/store.ts
+++ src/store.ts
@@ -1,32 +1,16 @@
 import { defineStore } from 'pinia'
 
-const versionString =
-  import.meta.env.MODE === 'development'
-    ? import.meta.env.VITE_APP_VERSION + '-dev'
-    : import.meta.env.VITE_APP_VERSION
-
 export const useStore = defineStore('main', {
   state: () => ({
-    debug: import.meta.env.MODE === 'development',
-    version: versionString,
-    isInitialized: false,
-    count: 0,
+    currentStep: 1,
   }),
 
   actions: {
-    initApp() {
-      this.isInitialized = true
-      console.log('app initialized!')
-    },
-
-    increment(value = 1) {
-      this.count += value
+    nextStep() {
+      this.currentStep++;
     },
   },
 
   getters: {
-    isReady: (state) => {
-      return !state.isInitialized
-    },
   },
 })
