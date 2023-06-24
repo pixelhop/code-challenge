2666c74539f16118bda36b546ba8082c40944313
Jennifer Bland <10262857+ratracegrad@users.noreply.github.com>
2023-06-24 19:37:50 -0400

    start on gridSize component. Collect input for grid size

--- src/components/GridSize.vue
+++ src/components/GridSize.vue
@@ -1,13 +1,33 @@
 <template>
-	<div>
-		<h1>Grid Size</h1>
-	</div>
+  <div>
+    <div class="mx-auto prose prose-indigo sm:rounded-md">
+      <h2>Grid Size</h2>
+      <p class="pb-4 text-xl leading-relaxed tracking-wide text-gray-700">
+        Enter the top right (or north east) coordinates of the rectangular
+        world. <br />Example: 5 3
+      </p>
+      <label
+        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        for="grid-size"
+      >
+        Enter Coordinates
+      </label>
+      <input
+        id="grid-size"
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+        type="text"
+        @input="handleInput($event)"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+// methods
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLButtonElement;
+  console.log('target', target.value);
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
