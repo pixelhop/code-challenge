53f6e447ae3d9436ff1ad6362ef9d5fba6c8b4e3
Jennifer Bland <10262857+ratracegrad@users.noreply.github.com>
2023-06-24 18:32:10 -0400

    Added a line to separate footer from rest of content

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,52 +1,54 @@
 <template>
-	<footer class="py-6 text-sm text-center text-gray-700">
-		<p>Created by Jennifer Bland</p>
-		<p>
-			<a href="mailto:ratracegrad@gmail.com">
-				<img
-					alt="Email logo"
-					class="inline mr-2"
-					src="@/assets/email.svg"
-					width="24"
-					height="24"
-				/>
-			</a>
-			<a href="https://www.jenniferbland.com/">
-				<img
-					alt="Website logo"
-					class="inline mr-2"
-					src="@/assets/website.svg"
-					width="24"
-					height="24"
-				/>
-			</a>
-			<a href="https://github.com/ratracegrad">
-				<img
-					alt="GitHub logo"
-					class="inline mr-2"
-					src="@/assets/github.svg"
-					width="24"
-					height="24"
-				/>
-			</a>
-			<a href="">
-				<img
-					alt="Twitter logo"
-					class="inline mr-2"
-					src="@/assets/twitter.svg"
-					width="24"
-					height="24"
-				/>
-			</a>
-			<a href="https://www.linkedin.com/in/ratracegrad/">
-				<img
-					alt="Linkedin logo"
-					class="inline"
-					src="@/assets/linkedin.svg"
-					width="24"
-					height="24"
-				/>
-			</a>
-		</p>
-	</footer>
+  <footer
+    class="py-6 text-sm text-center text-gray-700 mt-12 border-t-2 border-slate-400"
+  >
+    <p>Created by Jennifer Bland</p>
+    <p>
+      <a href="mailto:ratracegrad@gmail.com">
+        <img
+          alt="Email logo"
+          class="inline mr-2"
+          src="@/assets/email.svg"
+          width="24"
+          height="24"
+        />
+      </a>
+      <a href="https://www.jenniferbland.com/">
+        <img
+          alt="Website logo"
+          class="inline mr-2"
+          src="@/assets/website.svg"
+          width="24"
+          height="24"
+        />
+      </a>
+      <a href="https://github.com/ratracegrad">
+        <img
+          alt="GitHub logo"
+          class="inline mr-2"
+          src="@/assets/github.svg"
+          width="24"
+          height="24"
+        />
+      </a>
+      <a href="">
+        <img
+          alt="Twitter logo"
+          class="inline mr-2"
+          src="@/assets/twitter.svg"
+          width="24"
+          height="24"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/ratracegrad/">
+        <img
+          alt="Linkedin logo"
+          class="inline"
+          src="@/assets/linkedin.svg"
+          width="24"
+          height="24"
+        />
+      </a>
+    </p>
+  </footer>
 </template>
