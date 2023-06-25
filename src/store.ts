f18f3d34a2d34ce1c95af2751dc4de7fb7a84285
Jennifer Bland <10262857+ratracegrad@users.noreply.github.com>
2023-06-24 20:33:48 -0400

    Added state and actions for setting gridsize

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,23 @@ import { defineStore } from 'pinia'
 export const useStore = defineStore('main', {
   state: () => ({
     currentStep: 1,
+    isDisabled: false,
+    gridX: 5,
+    gridY: 3,
   }),
 
   actions: {
     nextStep() {
       this.currentStep++;
     },
+    setButtonDisabled(val: boolean) {
+      this.isDisabled = val;
+    },
+    setGridSize(gridX: number, gridY: number) {
+      this.gridX = gridX;
+      this.gridY = gridY;
+    },
   },
 
-  getters: {
-  },
-})
+  getters: {},
+});
